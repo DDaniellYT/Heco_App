3b2757b96a01fb698e25a6011542e36f660c438e
DDaniellYT <ocrisan23@gmail.com>
2024-10-15 16:06:29 +0300

    primitive requests panel done

--- server/server.js
+++ server/server.js
@@ -9,9 +9,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(express.json());
 
+const requests = [{subject:"testing"},{subject:"tss2"}];
+
 app.post('/login',(req,res)=>{
   if(req.body.user.name == 'admin') res.send({allowed: true});
   else res.send({allowed: false})
+})
+app.post('/request',(req,res)=>{
+  console.log(req.body);
+  requests.push(req.body);
+  res.send("");
+})
+app.get('/request',(req,res)=>{
+  console.log("get request");
+  console.log([...requests]);
+  res.send([...requests])
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
