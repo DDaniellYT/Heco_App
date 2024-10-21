74f89a4e3f8f4ca596106778eb965b142344e913
DDaniellYT <ocrisan23@gmail.com>
2024-10-21 20:38:46 +0300

    requests and info screen revamp

--- server/server.js
+++ server/server.js
@@ -9,21 +9,24 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(express.json());
 
-const requests = [{subject:"testing"},{subject:"tss2"}];
+var requests = [{subject:"testing"},{subject:"tss2"}];
 
 app.post('/login',(req,res)=>{
   if(req.body.user.name == 'admin') res.send({allowed: true});
   else res.send({allowed: false})
 })
-app.post('/request',(req,res)=>{
-  console.log(req.body);
+app.post('/requestItem',(req,res)=>{
   requests.push(req.body);
+  res.send("");
+})
+app.post('/requestArr',(req,res)=>{
+  requests = req.body;
   res.send("");
 })
 app.get('/request',(req,res)=>{
   console.log("get request");
-  console.log([...requests]);
-  res.send([...requests])
+  console.log(requests);
+  res.send(requests)
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
