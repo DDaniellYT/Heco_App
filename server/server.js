0be3d82718ad98ae5166ade50e8c5f06dc22c92f
DDaniellYT <ocrisan23@gmail.com>
2024-10-21 21:28:59 +0300

    requests and info screen revamp

--- server/server.js
+++ server/server.js
@@ -10,24 +10,39 @@ app.use(cors());
 app.use(express.json());
 
 var requests = [{subject:"testing"},{subject:"tss2"}];
+var accepted = [{subject:"ttt1"},{subject:"dettt2"}];
 
 app.post('/login',(req,res)=>{
   if(req.body.user.name == 'admin') res.send({allowed: true});
   else res.send({allowed: false})
-})
+});
+
 app.post('/requestItem',(req,res)=>{
   requests.push(req.body);
   res.send("");
-})
+});
 app.post('/requestArr',(req,res)=>{
   requests = req.body;
   res.send("");
-})
+});
 app.get('/request',(req,res)=>{
   console.log("get request");
-  console.log(requests);
   res.send(requests)
-})
+});
+
+app.post('/acceptedItem',(req,res)=>{
+  console.log(req.body);
+  accepted.push(req.body)
+  res.send("");
+});
+app.post('/acceptedArr',(req,res)=>{
+  accepted = req.body;
+  res.send("");
+});
+app.get('/accepted',(req,res)=>{
+  console.log('get accepted');
+  res.send(accepted);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
