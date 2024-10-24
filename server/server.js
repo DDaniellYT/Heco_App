142e7f30d233e778387e41d05d8e2db07f7b1a2d
DDaniellYT <ocrisan23@gmail.com>
2024-10-24 16:56:51 +0300

    finished quick stats and so the info requests part && added profile space

--- server/server.js
+++ server/server.js
@@ -9,40 +9,102 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(express.json());
 
-var requests = [{subject:"testing"},{subject:"tss2"}];
-var accepted = [{subject:"ttt1"},{subject:"dettt2"}];
+var requests = [];
+var accepted = [];
+
+var countHRRequests = 0;
+var countMechRequests = 0;
+var countChemRequests = 0;
+var countWorkRequests = 0;
+var countSecRequests = 0;
+var countCleanRequests = 0;
+
+var countHRAccepted = 0;
+var countMechAccepted = 0;
+var countChemAccepted = 0;
+var countWorkAccepted = 0;
+var countSecAccepted = 0;
+var countCleanAccepted = 0;
 
 app.post('/login',(req,res)=>{
   if(req.body.user.name == 'admin') res.send({allowed: true});
-  else res.send({allowed: false})
+  else res.send({allowed: false});
 });
 
-app.post('/requestItem',(req,res)=>{
+app.post('/requestAdd',(req,res)=>{
   requests.push(req.body);
+  switch(req.body.reciever){
+    case 'HResources':countHRRequests++;break;
+    case 'Mechanics':countMechRequests++;break;
+    case 'Chemists':countChemRequests++;break;
+    case 'Workers':countWorkRequests++;break;
+    case 'Security':countSecRequests++;break;
+    case 'Cleaning':countCleanRequests++;break;
+  };
   res.send("");
 });
-app.post('/requestArr',(req,res)=>{
-  requests = req.body;
+app.post('/requestRemove',(req,res)=>{
+  console.log(requests);
+  requests.splice(req.body.index,1);
+  console.log(requests);
+  console.log(req.body);
+  switch(req.body.reciever){
+    case 'HResources':countHRRequests-=1;break;
+    case 'Mechanics':countMechRequests-=1;break;
+    case 'Chemists':countChemRequests-=1;break;
+    case 'Workers':countWorkRequests-=1;break;
+    case 'Security':countSecRequests-=1;break;
+    case 'Cleaning':countCleanRequests-=1;break;
+  };
+
+  console.log(countHRRequests, countMechRequests, countChemRequests, countWorkRequests, countSecRequests, countCleanRequests, countHRAccepted, countMechAccepted, countChemAccepted, countWorkAccepted, countSecAccepted, countCleanAccepted);
+
   res.send("");
 });
-app.get('/request',(req,res)=>{
-  console.log("get request");
-  res.send(requests)
+app.post('/acceptedAdd',(req,res)=>{
+  accepted.push(req.body);
+  switch(req.body.reciever){
+    case 'HResources':countHRAccepted++;break;
+    case 'Mechanics':countMechAccepted++;break;
+    case 'Chemists':countChemAccepted++;break;
+    case 'Workers':countWorkAccepted++;break;
+    case 'Security':countSecAccepted++;break;
+    case 'Cleaning':countCleanAccepted++;break;
+  };
+  res.send("");
+});
+app.post('/acceptedRemove',(req,res)=>{
+  // accepted.splice();
+  // requests.splice(req.body.index,1);
+  // switch(req.body.reciever){
+  //   case 'HResources':countHRAccepted++;break;
+  //   case 'Mechanics':countMechAccepted++;break;
+  //   case 'Chemists':countChemAccepted++;break;
+  //   case 'Workers':countWorkAccepted++;break;
+  //   case 'Security':countSecAccepted++;break;
+  //   case 'Cleaning':countCleanAccepted++;break;
+  // };
+  res.send("");
+});
+app.get('/lists',(req,res)=>{
+  res.send({
+    requests,
+    accepted,
+    countHRAccepted,
+    countHRRequests,
+    countMechAccepted,
+    countMechRequests,
+    countChemAccepted,
+    countChemRequests,
+    countWorkAccepted,
+    countWorkRequests,
+    countSecAccepted,
+    countSecRequests,
+    countCleanAccepted,
+    countCleanRequests
+  });
 });
 
-app.post('/acceptedItem',(req,res)=>{
-  console.log(req.body);
-  accepted.push(req.body)
-  res.send("");
-});
-app.post('/acceptedArr',(req,res)=>{
-  accepted = req.body;
-  res.send("");
-});
-app.get('/accepted',(req,res)=>{
-  console.log('get accepted');
-  res.send(accepted);
-});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
